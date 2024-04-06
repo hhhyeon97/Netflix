bb71a7cceca162d3aa96d8f365411028512e210e
jihyeon <kth083030@naver.com>
2024-04-06 22:30:03 +0900

    nested route/outlet

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import AppLayout from './layout/AppLayout';
+import Homepage from './pages/Homepage/Homepage';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
+import MoviePage from './pages/Movies/MoviePage';
+import MovieDetail from './pages/MovieDetail/MovieDetail';
+
+// 홈페이지
+// 영화 전체 페이지 (검색)
+// 영화 디테일 페이지
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<AppLayout />} />
+        <Route index element={<Homepage />} />
+        <Route path="movies">
+          <Route index element={<MoviePage />} />
+          <Route path=":id" element={<MovieDetail />} />
+        </Route>
+
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
