0e7a5f549d4e0150da6656ecb0754d034bf76f39
jihyeon <kth083030@naver.com>
2024-04-06 23:41:17 +0900

    404page

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,7 +1,77 @@
 import React from 'react';
+import './NotFoundPage.style.css';
 
 const NotFoundPage = () => {
-  return <div>404</div>;
+  return (
+    <div className="not-wrap">
+      <div className="svg-container"></div>
+      <div className="title-container">
+        <h1 className="title">404</h1>
+        <svg
+          className="svg-icon"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="-1.785 -1.785 49.98 49.98"
+          height="170"
+          width="170"
+          id="Threat-Monitor--Streamline-Flex"
+        >
+          <desc>Threat Monitor Streamline Icon: https://streamlinehq.com</desc>
+          <g id="threat-monitor--bug-monitor-file-threat-danger-computer">
+            <path
+              id="Intersect"
+              stroke="#ffffff"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M21.5475 2.89731c-5.294055 0.036465 -10.4754 0.2652 -15.34845 0.8817900000000001a3.12273 3.12273 0 0 0 -2.6619450000000002 2.3868C2.74482 9.62013 2.48625 12.706395 2.48625 18.71649c0 5.758155 0.663 8.8842 1.2066599999999998 12.149475 0.24530999999999997 1.468545 1.3922999999999999 2.64537 2.8674749999999998 2.821065 5.2807949999999995 0.62985 10.90635 0.85527 16.644614999999998 0.85527 5.310630000000001 0 10.43562 -0.19227 15.401489999999999 -0.719355"
+              stroke-width="3.57"
+            ></path>
+            <path
+              id="Rectangle 23"
+              stroke="#ffffff"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="m28.081364999999998 34.5423 2.1216 8.977020000000001H16.207035l2.118285 -8.977020000000001"
+              stroke-width="3.57"
+            ></path>
+            <path
+              id="Vector 8"
+              stroke="#ffffff"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M10.80027 43.51932h24.812775000000002"
+              stroke-width="3.57"
+            ></path>
+            <path
+              id="Vector"
+              stroke="#ffffff"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M24.942059999999998 25.70451H20.73864C22.62819 16.193775 25.20063 9.97152 32.334509999999995 2.9172c6.891885 7.15377 9.388079999999999 13.143975 11.59587 22.787309999999998H40.14465"
+              stroke-width="3.57"
+            ></path>
+            <path
+              id="Vector 501"
+              stroke="#ffffff"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="m32.331195 14.310855 0 4.641"
+              stroke-width="3.57"
+            ></path>
+            <path
+              id="Vector 502"
+              stroke="#ffffff"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="m32.331195 24.905594999999998 0 0.7989149999999999"
+              stroke-width="3.57"
+            ></path>
+          </g>
+        </svg>
+        <p>PAGE NOT FOUND</p>
+      </div>
+    </div>
+  );
 };
 
 export default NotFoundPage;
