82d14c109131178abd496200fccb1a9546c29d55
jihyeon <kth083030@naver.com>
2024-04-07 18:12:17 +0900

    card

--- src/pages/Homepage/components/MovieCard/MovieCard.jsx
+++ src/pages/Homepage/components/MovieCard/MovieCard.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import { Badge } from 'react-bootstrap';
+import './MovieCard.style.css';
 
-const MovieCard = () => {
-  return <div>MovieCard</div>;
+const MovieCard = ({ movie }) => {
+  return (
+    <div
+      className="movie-card"
+      style={{
+        backgroundImage:
+          'url(' +
+          `https://image.tmdb.org/t/p/w600_and_h900_bestv2${movie.poster_path}` +
+          ')',
+      }}
+    >
+      <div className="overlay">
+        <h1>{movie.title}</h1>
+        {movie.genre_ids.map((id) => (
+          <Badge bg="danger">{id}</Badge>
+        ))}
+      </div>
+      <div>
+        <div>{movie.vote_average}</div>
+        <div>{movie.popularity}</div>
+        <div>{movie.adult ? 'over18' : 'under18'}</div>
+      </div>
+    </div>
+  );
 };
 
 export default MovieCard;
