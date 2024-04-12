08a3df3d05d24dafe1b19941a6bb183f06a5e8fa
jihyeon <kth083030@naver.com>
2024-04-13 03:17:48 +0900

    수정커밋

--- src/common/MovieTag/MovieTag.jsx
+++ src/common/MovieTag/MovieTag.jsx
@@ -7,16 +7,25 @@ const MovieTag = ({ movie }) => {
   return (
     <div className="detail-tag-wrap">
       <div className="movie-vote">
-        <FontAwesomeIcon icon={faStar} color="#ebedad" /> {movie.vote_average}
+        <FontAwesomeIcon icon={faStar} color="#ebedad" />{' '}
+        {movie.vote_average.toFixed(2)}
       </div>
       <div className="movie-popular">
-        <FontAwesomeIcon icon={faThumbsUp} /> {movie.popularity}
+        <FontAwesomeIcon icon={faThumbsUp} color="#797b91" />{' '}
+        {movie.popularity.toFixed(2)}
       </div>
-      <div className="movie-adult">
+      {/* <div className="movie-adult">
         {movie.adult ? (
           <span className="adult_19">19</span>
         ) : (
           <span className="adult_all">ALL</span>
+        )}
+      </div> */}
+      <div>
+        {movie.adult ? (
+          <span className="movie-age-19">19</span>
+        ) : (
+          <span className="movie-age-all">ALL</span>
         )}
       </div>
     </div>
